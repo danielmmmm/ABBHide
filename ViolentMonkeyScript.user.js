5f2cadc4fe5236a70eee5f8c44562110d723b344
danielmmmm <daniel.schaaaf@gmail.com>
2023-03-01 22:14:23 +0100

    Update ViolentMonkeyScript.user.js
    By default, blocked audio-book releases are now shown as a link with the removal reason below the ABB navigation bar. These links load the corresponding torrent release page, that displays normally now. In previous versions, blocking would also remove content from these torrent release pages.
    
    On the torrent release page, user comments can be blocked. This works similarly to blocking releases on the ABB main page. Finally, you can silence the racist, misogynist, antisemitic, ..., braindead edgelords! 😁

--- ViolentMonkeyScript.user.js
+++ ViolentMonkeyScript.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Hide releases (categories/users) on ABB
 // @description    Hide releases in specific categories or by specific users on ABB
-// @version        0.37
+// @version        0.40
 // @author         mseitz
 // @namespace      https://greasyfork.org/en/scripts/443140-hide-releases-on-abb
 // @license        MIT
@@ -16,12 +16,12 @@
 
 //Explanation of user values
 //
-//--blocklistCategory/blocklistUser--
+//--blocklistCategory/blocklistUser/blocklistUserComment--
 // Contains ABB categories/usernames to block. The categories/usernames are case sensitive, must be comma seperated, and must not have leading or trailing withe-space.
 // The whole string must be in double quotation marks.
 // Example: "user1,User2,USER3" or "Some Category, Category2,Some other category"
 //
-//--placeholderUser/placeholderCategory--
+//--placeholderUser/placeholderCategory/placeholderUserComment--
 // Determines if postings are removed completely, or if a placeholder is shown instead.
 // Possible values for show are:
 //  0  removes postings completely
@@ -56,6 +56,9 @@ var blockedUserArr;
 var showRemovalUser;
 var blockedCategoryArr;
 var showRemovalCategory;
+var blockedUserCommentArr;
+var showRemovalUserComment;
+
 
 if (GM_getValue("blocklistUser") == null ) {
   GM_setValue("blocklistUser","Replace this text with usernames you want to block, separated by a comma");
@@ -101,32 +104,75 @@ if (GM_getValue("placeholderCategory") == null ) {
   }
 }
 
-
-//Block category - read blocklist into array and remove respective div
-for (var j = 0; j < blockedCategoryArr.length; j++) {
-  if (showRemovalCategory == 0) {
-    $('div.postInfo:contains(' + blockedCategoryArr[j] + ')').parent("div").remove();
-  } else if (showRemovalCategory == 1) {
-    $('div.postInfo:contains(' + blockedCategoryArr[j] + ')').parent("div").replaceWith("<div class='postInfo'>(Posting removed by \"Hide releases on ABB\")<br><br></div>");
-  } else if (showRemovalCategory == 2) {
-    $('div.postInfo:contains(' + blockedCategoryArr[j] + ')').parent("div").replaceWith("<div class='postInfo'>(Posting of category \"" + blockedCategoryArr[j] + "\" removed; only first matched category shown)<br><br></div>");
-  } else {
-    unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"placeholderCategory\" must be null, 0, 1, or 3.");
+if (GM_getValue("blocklistUserComment") == null ) {
+  GM_setValue("blocklistUserComment","Replace this text with usernames whose comments you want to block, separated by a comma");
+} else {
+  try{
+    blockedUserCommentArr = GM_getValue("blocklistUserComment").split(",");
+  }
+  catch(e){
+    unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"blocklistUserComment\" must be of format \"user1,user2,user3\".");
   }
 }
 
 
-//Block user - read blocklist into array and remove respective div
-for (var i = 0; i < blockedUserArr.length; i++) {
-  if (showRemovalUser == 0) {
-    $("div.postContent:contains(" + blockedUserArr[i] + ")").parent("div").remove();
-  } else if (showRemovalUser == 1) {
-    $("div.postContent:contains(" + blockedUserArr[i] + ")").parent("div").replaceWith("<div class='postInfo'>(Posting removed by \"Hide releases on ABB\")<br><br></div>");
-  } else if (showRemovalUser == 2) {
-    $("div.postContent:contains(" + blockedUserArr[i] + ")").parent("div").replaceWith("<div class='postInfo'>(Posting by user " + blockedUserArr[i] + " removed)<br><br></div>");
-  } else {
-    unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"show\" must be null, 0, 1, or 3.");
+if (GM_getValue("placeholderUserComment") == null ) {
+  GM_setValue("placeholderUserComment",2);
+} else {
+  try{
+    showRemovalUserComment = GM_getValue("placeholderUserComment");
   }
+  catch(e){
+    unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"placeholderUserComment\" must be 0, 1, or 2.");
+  }
+}
+
+
+if (window.location.href.includes("audio-books")) {
+
+  //Block user comment - read blocklist into array and remove or modify respective li
+  for (var i = 0; i < blockedUserCommentArr.length; i++) {
+    if (showRemovalUserComment == 0) {
+      $("span.commentAuthor:contains(" + blockedUserCommentArr[i] + ")").closest("li").remove();
+    } else if (showRemovalUserComment == 1) {
+      $("span.commentAuthor:contains(" + blockedUserCommentArr[i] + ")").closest("li").replaceWith("<li class='alt'><div class='reviewBody'><div itemprop='reviewBody'><p>(Comment removed by 'Hide releases on ABB')</p></div></div></li>");
+    } else if (showRemovalUserComment == 2) {
+      $("span.commentAuthor:contains(" + blockedUserCommentArr[i] + ")").closest("li").replaceWith("<li class='alt'><div class='reviewBody'><div itemprop='reviewBody'><p>(Comment by user " + blockedUserCommentArr[i] + " removed)</p></div></div></li>");
+    } else {
+      unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"showRemovalUserComment\" must be null, 0, 1, or 3.");
+    }
+  }
+
+} else {
+
+  //Block category - read blocklist into array and remove or modify respective div
+  for (var j = 0; j < blockedCategoryArr.length; j++) {
+    if (showRemovalCategory == 0) {
+      $('div.postInfo:contains(' + blockedCategoryArr[j] + ')').parent("div").remove();
+    } else if (showRemovalCategory == 1) {
+      $('div.postInfo:contains(' + blockedCategoryArr[j] + ')').parent("div").replaceWith("<div class='postInfo'>(Posting removed by 'Hide releases on ABB')<br><br></div>");
+    } else if (showRemovalCategory == 2) {
+      $("div.postInfo:contains(" + blockedCategoryArr[j] + ")").parent().find("h2").find("a").clone().appendTo("div#content").before("<br>").append(" <span style='color:#0000FF'>(Release of category </span><span style='color:#FF0000'>" + blockedCategoryArr[j] + "</span><span style='color:#0000FF'> removed)</span><br>").wrap("<div class='blocked' style='font-size: 10px'></div>");
+      $("div.postInfo:contains(" + blockedCategoryArr[j] + ")").parent('div').remove();
+    } else {
+      unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"placeholderCategory\" must be null, 0, 1, or 3.");
+    }
+  }
+
+  //Block user - read blocklist into array and remove or modify respective div
+  for (var k = 0; k < blockedUserArr.length; k++) {
+    if (showRemovalUser == 0) {
+      $("div.postContent:contains(" + blockedUserArr[k] + ")").parent("div").remove();
+    } else if (showRemovalUser == 1) {
+      $("div.postContent:contains(" + blockedUserArr[k] + ")").parent("div").replaceWith("<div class='postInfo'>(Posting removed by 'Hide releases on ABB')<br><br></div>");
+    } else if (showRemovalUser == 2) {
+      $("div.postContent:contains(" + blockedUserArr[k] + ")").parent().find("h2").find("a").clone().appendTo("div#content").before("<br>").append(" <span style='color:#0000FF'>(Release by user </span><span style='color:#FF0000'>" + blockedUserArr[k] + "</span><span style='color:#0000FF'> removed)</span><br>").wrap("<div class='blocked' style='font-size: 10px'></div>");
+      $("div.postContent:contains(" + blockedUserArr[k] + ")").parent("div").remove();
+    } else {
+      unsafeWindow.console.error("UserScript \"Hide releases on ABB\" value \"showRemovalUser\" must be null, 0, 1, or 3.");
+    }
+  }
+
 }
 
 
